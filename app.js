8c66f4ae5b7008b2ec6fbb968d90027238c84d45
David Ph <davidphang431@gmail.com>
2021-04-17 08:07:10 +0700

    Fixed some front end stuff to fit with bootstrap

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
+/////////////////////
+// REQUIRE PACKAGE //
+/////////////////////
+
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+////////////////////////////////////
+// SERVER SETTING /////////////////
+///////////////////////////////////
+
+mongoose.connect('mongodb://localhost:27017/coffees', { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(express.static('public'));
+
+
+///////////////////////////////////
+// PORT SETTING ////////////////////
+///////////////////////////////////
 
 const PORT = 3000;
 app.listen(PORT, ()=> console.log('Listening to 3000....'));
