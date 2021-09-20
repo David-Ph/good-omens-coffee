3304dcc3ad0473882d39aa85e71f54332c7bdbc2
David <davidphang431@gmail.com>
2021-09-20 13:04:31 +0700

    Fix some UI issue

--- app.js
+++ app.js
@@ -2,47 +2,52 @@
 // REQUIRE PACKAGE //
 /////////////////////
 
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const multer = require('multer');
-const beansRouter = require('./routes/beans');
-const ordersRouter = require('./routes/orders');
-const session = require('express-session');
-const passport = require('passport');
-const User = require('./models/users').User;
-const passportLocalMongoose = require('passport-local-mongoose');
-const usersRouter = require('./routes/users');
-const checkAuth = require('./middleware/auth');
+const mongoose = require("mongoose");
+const multer = require("multer");
+const beansRouter = require("./routes/beans");
+const ordersRouter = require("./routes/orders");
+const session = require("express-session");
+const passport = require("passport");
+const User = require("./models/users").User;
+const passportLocalMongoose = require("passport-local-mongoose");
+const usersRouter = require("./routes/users");
+const checkAuth = require("./middleware/auth");
 
 ////////////////////////////////////
 // SERVER SETTING /////////////////
 ///////////////////////////////////
 // GENERAL SETTING
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(express.json());
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 // DOTENV
 let apiKey = process.env.API_KEY;
 let secretSession = process.env.SECRET_SESSION;
 
 // express-session
-app.use(session({
+app.use(
+  session({
     secret: `${secretSession}`,
     resave: false,
     saveUninitialized: true,
     // cookie: { secure: true }
-  }))
+  })
+);
 
 // Initialize Passport
 app.use(passport.initialize());
 app.use(passport.session());
 
 // MONGODB
-mongoose.connect('mongodb://localhost:27017/coffees', { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.set('useCreateIndex', true);
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+mongoose.set("useCreateIndex", true);
 
 // passport-local
 passport.use(User.createStrategy());
@@ -51,32 +56,32 @@ passport.deserializeUser(User.deserializeUser());
 
 // multer setting
 let imageStorage = multer.diskStorage({
-    destination: (req, file, cb) => cb(null, 'public/img/coffeeBags'),
-    filename: (req, file, cb) => cb(null, file.originalname)
-})
-app.use(multer({storage: imageStorage}).single('imageFile'));
+  destination: (req, file, cb) => cb(null, "public/img/coffeeBags"),
+  filename: (req, file, cb) => cb(null, file.originalname),
+});
+app.use(multer({ storage: imageStorage }).single("imageFile"));
 
 ///////////////////////////////////
 // ROUTES ////////////////////
 ///////////////////////////////////
 
-app.use('/beans', beansRouter);
-app.use('/orders', ordersRouter);
-app.use('/users', usersRouter);
+app.use("/beans", beansRouter);
+app.use("/orders", ordersRouter);
+app.use("/users", usersRouter);
 
-app.get('/admin', checkAuth, (req, resp) =>{
-    resp.render('admin');
-})
-app.get('/getApiKey', (req, resp) => {
-    resp.send(apiKey);
-})
-app.get('/login', (req, resp) =>{
-    resp.render('login');
-})
+app.get("/admin", checkAuth, (req, resp) => {
+  resp.render("admin");
+});
+app.get("/getApiKey", (req, resp) => {
+  resp.send(apiKey);
+});
+app.get("/login", (req, resp) => {
+  resp.render("login");
+});
 
 ///////////////////////////////////
 // PORT SETTING ////////////////////
 ///////////////////////////////////
 
 const PORT = 3000;
-app.listen(PORT, ()=> console.log('Listening to 3000....'));
+app.listen(PORT, () => console.log("Listening to 3000...."));
--- public/admin/js/create-beans.js
+++ public/admin/js/create-beans.js
@@ -1,35 +1,48 @@
-let createForm = document.querySelector('.create-bean-form');
-let createOrigin = document.querySelector('#create-origin');
-let createRoast = document.querySelector('#create-roast');
-let createNotes = document.querySelector('#create-notes');
-let createPrice = document.querySelector('#create-price');
-let createImageUrl = document.querySelector('#create-image-url');
-let createImageFile = document.querySelector('#create-image-file');
+let createForm = document.querySelector(".create-bean-form");
+let createOrigin = document.querySelector("#create-origin");
+let createRoast = document.querySelector("#create-roast");
+let createNotes = document.querySelector("#create-notes");
+let createPrice = document.querySelector("#create-price");
+let createImageUrl = document.querySelector("#create-image-url");
+let createImageFile = document.querySelector("#create-image-file");
 
+createForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let data = new FormData();
+  data.append("origin", createOrigin.value);
+  data.append("roast", createRoast.value);
+  data.append("notes", createNotes.value);
+  data.append("price", createPrice.value);
+  data.append("imageUrl", createImageUrl.value);
+  data.append("imageFile", createImageFile.files[0]);
 
-createForm.addEventListener('submit', (e) =>{
-    e.preventDefault();
-    let data = new FormData();
-    data.append('origin', createOrigin.value);
-    data.append('roast', createRoast.value);
-    data.append('notes', createNotes.value);
-    data.append('price', createPrice.value);
-    data.append('imageUrl', createImageUrl.value);
-    data.append('imageFile', createImageFile.files[0]);
+  fetch("http://localhost:3000/beans", {
+    method: "POST",
+    body: data,
+  })
+    .then((response) => response.text())
+    .then((data) => {
+      createOrigin.value = "";
+      createRoast.value = "";
+      createNotes.value = "";
+      createPrice.value = "";
+      createImageUrl.value = "";
 
-    fetch('http://localhost:3000/beans', {
-        method: 'POST', 
-        body: data
-    }).then((response) => response.text()).then((data) => window.history.go());
+      window.history.go();
+    });
 });
 
-function disableInput(input1, input2){
-    if(input1.value){
-        input2.disabled = true;
-    }else{
-        input2.disabled = false;
-    }
+function disableInput(input1, input2) {
+  if (input1.value) {
+    input2.disabled = true;
+  } else {
+    input2.disabled = false;
+  }
 }
 
-createImageUrl.addEventListener('change', function() {disableInput(this, createImageFile)});
-createImageFile.addEventListener('change', function() {disableInput(this, createImageUrl)});
+createImageUrl.addEventListener("change", function () {
+  disableInput(this, createImageFile);
+});
+createImageFile.addEventListener("change", function () {
+  disableInput(this, createImageUrl);
+});
